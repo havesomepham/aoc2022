df349208fe149a36e7ec2db2dd5f8783b3869524
havesomepham <23087717+havesomepham@users.noreply.github.com>
2022-12-06 13:25:15 -0500

    Day 6

---
+++ day6/Day6B.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Day6B {
+    public static void main(String[] args) throws IOException {
+        Scanner reader = new Scanner(new File("input.txt"));
+        String data = reader.nextLine();
+        String packet;
+        boolean noRepeats = false;
+        int N = 14;
+        int index = N;
+
+        checkChars: while (!noRepeats) {
+            packet = data.substring(index - N, index);
+            char[] letters = packet.toCharArray();
+            for (int i = 0; i < N; i++) {
+                for (int j = i + 1; j < N; j++) {
+                    if (letters[i] == letters[j]) {
+                        index++;
+                        continue checkChars;
+                    }
+                }
+            }
+            noRepeats = true;
+        }
+        System.out.println(index);
+    }
+}
